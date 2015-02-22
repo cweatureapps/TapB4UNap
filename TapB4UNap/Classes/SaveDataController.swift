e37c7251f11457c3eb651a784cf5c7449c9ccc90
Ken Ko <ken.ko.au@gmail.com>
2015-02-23 01:47:34 +1100

    adding flickr image attribution, and making an about message that animates in and out

--- TapB4UNap/Classes/SaveDataController.swift
+++ TapB4UNap/Classes/SaveDataController.swift
@@ -11,12 +11,20 @@ import UIKit
 
 class SaveDataController : UIViewController {
 
+    // MARK: privates
+    
     private let timeKeeper = TimeKeeper()
 
     @IBOutlet weak private var statusMessageLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var adjustSleepButton: UIButton!
-
+    @IBOutlet weak private var timeLabel: UILabel!
+    @IBOutlet weak private var adjustSleepButton: UIButton!
+    @IBOutlet weak private var aboutButton: UIButton!
+    @IBOutlet weak private var copyrightButton: UILabel!
+    @IBOutlet weak private var aboutBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak private var copyrightBottomConstraint: NSLayoutConstraint!
+    
+    // MARK: controller code
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -63,6 +71,7 @@ class SaveDataController : UIViewController {
 
 
     // MARK: unwind methods
+    
     @IBAction func cancelToSaveDataController(segue: UIStoryboardSegue) {
     }
     
@@ -88,4 +97,32 @@ class SaveDataController : UIViewController {
         }
     }
     
+    // MARK: copyright animation
+    
+    @IBAction func showCopyrightLabel(sender: UIButton) {
+        swapControls(contraintToMoveOffScreen: aboutBottomConstraint, constraintToMoveOnScreen: copyrightBottomConstraint)
+    }
+    
+    @IBAction func showAboutButton(sender: AnyObject) {
+        swapControls(contraintToMoveOffScreen: copyrightBottomConstraint, constraintToMoveOnScreen: aboutBottomConstraint)
+    }
+    
+    private func swapControls(#contraintToMoveOffScreen:NSLayoutConstraint , constraintToMoveOnScreen:NSLayoutConstraint) {
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(0.3,
+            animations: {
+                contraintToMoveOffScreen.constant = -100
+                self.view.layoutIfNeeded()
+            },
+            completion: {
+                finished in
+                if finished {
+                    self.view.layoutIfNeeded()
+                    UIView.animateWithDuration(0.3) {
+                        constraintToMoveOnScreen.constant = 20
+                        self.view.layoutIfNeeded()
+                    }
+                }
+            })
+    }
 }
